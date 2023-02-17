5d6aa2ccf014d93672e23331ad743147d9e6ccb0
natanaelhn <nainggolannainggolan74@gmail.com>
2023-02-17 23:06:51 +0700

    menambahkan navigasi ke landing_page_screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,12 +1,37 @@
 
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_futsal_gembira/screen/landing_page_screen.dart';
 import 'package:flutter_application_futsal_gembira/style/color_style.dart';
 import 'package:flutter_application_futsal_gembira/style/font_weight.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  late Timer timer;
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer(const Duration(milliseconds: 1500), () {
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const LandindPageScreen(),));
+    });
+  }
+
+  @override
+  void dispose() {
+    timer.cancel();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
