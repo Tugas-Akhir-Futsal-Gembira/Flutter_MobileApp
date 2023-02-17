16cb840717571b6693255c6fe82b5e87c7a4a2be
natanaelhn <nainggolannainggolan74@gmail.com>
2023-02-17 22:10:01 +0700

    membuat splash screen

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,14 +1,32 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:flutter_application_futsal_gembira/style/color_style.dart';
+import 'package:flutter_application_futsal_gembira/style/font_weight.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
+    return Scaffold(
+      backgroundColor: primaryBaseColor,
+      body: Center(
+        child: FittedBox(
+          fit: BoxFit.scaleDown,
+          child: Padding(
+            padding: const EdgeInsets.all(64.0),
+            child: Text(
+              'Futsal\nGembira', 
+              style: TextStyle(
+                color: Colors.white, 
+                fontWeight: semiBold, 
+                fontSize: 40,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
