fed665444fccd777d68039ffef2d85458127df33
natanaelhn <nainggolannainggolan74@gmail.com>
2023-03-28 21:22:30 +0700

    menambahkan refresh indicator pada beranda_screen dan riwayat_penyewaan_screen

--- lib/screen/beranda/beranda_screen.dart
+++ lib/screen/beranda/beranda_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_application_futsal_gembira/model/penyewaan/sudah_dibayar_model.dart';
 import 'package:flutter_application_futsal_gembira/screen/beranda/widget/field_gridview.dart';
@@ -5,44 +7,86 @@ import 'package:flutter_application_futsal_gembira/screen/beranda/widget/welcome
 import 'package:flutter_application_futsal_gembira/style/color_style.dart';
 import 'package:flutter_application_futsal_gembira/widget/penyewaan_container.dart';
 
-class BerandaScreen extends StatelessWidget {
+class BerandaScreen extends StatefulWidget {
   const BerandaScreen({super.key});
+
+  @override
+  State<BerandaScreen> createState() => _BerandaScreenState();
+}
+
+class _BerandaScreenState extends State<BerandaScreen> {
+
+  ValueNotifier<bool> isLoading = ValueNotifier(true);
+
+  @override
+  void initState() {
+    super.initState();
+    refresh();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       color: primaryBaseColor,
-      child: Column(
-        children: [
-          Container(
-            height: 1,
-            color: primaryLightestColor,
-          ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16),
-                child: Column(
-                  children: [
-                    // WelcomeContainer(name: 'Chandra',),
-                    PenyewaanContainer(
-                      abstractPenyewaanModel: SudahDibayarModel(
-                        fieldName: 'Lapangan#1', 
-                        rentDateTime: DateTime.now().add(const Duration(hours: 2)), 
-                        durationInt: 2, 
-                        createdAtDateTime: DateTime.now(),
-                      ),
-                      customMessage: 'Semoga Chandra mendapat pengalaman menyenangkan',
+      child: ValueListenableBuilder(
+        valueListenable: isLoading,
+        builder: (context, value, child) {
+          if(value){
+            return LinearProgressIndicator(
+              color: infoColor,
+              backgroundColor: primaryLightestColor,
+            );
+          }
+          else{
+            return child!;
+          }
+        },
+        child: Column(
+          children: [
+            Container(
+              height: 1,
+              color: primaryLightestColor,
+            ),
+            Expanded(
+              child: RefreshIndicator(
+                onRefresh: () async{
+                  refresh();
+                },
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Column(
+                      children: [
+                        // WelcomeContainer(name: 'Chandra',),
+                        PenyewaanContainer(
+                          abstractPenyewaanModel: SudahDibayarModel(
+                            fieldName: 'Lapangan#1', 
+                            rentDateTime: DateTime.now().add(const Duration(hours: 2)), 
+                            durationInt: 2, 
+                            createdAtDateTime: DateTime.now(),
+                          ),
+                          customMessage: 'Semoga Chandra mendapat pengalaman menyenangkan',
+                        ),
+                        const SizedBox(height: 16,),
+                        const FieldGridView(name: 'Chandra'),
+                      ],
                     ),
-                    const SizedBox(height: 16,),
-                    const FieldGridView(name: 'Chandra'),
-                  ],
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        )
       ),
+    );
+  }
+
+  void refresh(){
+    isLoading.value = true;
+
+    Timer( 
+      const Duration(seconds: 1), 
+      ()=> isLoading.value = false,
     );
   }
 }
