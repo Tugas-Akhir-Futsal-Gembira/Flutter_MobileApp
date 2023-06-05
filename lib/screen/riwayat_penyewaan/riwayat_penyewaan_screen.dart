4e66b7c065e476ecbeeba0b760ea8218fc2420f7
natanaelhn <nainggolannainggolan74@gmail.com>
2023-06-05 13:50:00 +0700

    mengerjakan tampilan listView yang dapat extendable

--- lib/screen/riwayat_penyewaan/riwayat_penyewaan_screen.dart
+++ lib/screen/riwayat_penyewaan/riwayat_penyewaan_screen.dart
@@ -3,11 +3,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_futsal_gembira/model/json_model.dart';
 import 'package:flutter_application_futsal_gembira/model/penyewaan/abstract_penyewaan_model.dart';
-import 'package:flutter_application_futsal_gembira/model/penyewaan/menunggu_pembayaran_model.dart';
-import 'package:flutter_application_futsal_gembira/model/penyewaan/sudah_dibayar_model.dart';
 import 'package:flutter_application_futsal_gembira/service/gate_service.dart';
 import 'package:flutter_application_futsal_gembira/style/color_style.dart';
 import 'package:flutter_application_futsal_gembira/style/font_weight.dart';
+import 'package:flutter_application_futsal_gembira/widget/custom_snackbar.dart';
 import 'package:flutter_application_futsal_gembira/widget/penyewaan_container.dart';
 
 class RiwayatPenyewaanScreen extends StatefulWidget {
@@ -20,6 +19,8 @@ class RiwayatPenyewaanScreen extends StatefulWidget {
 class _RiwayatPenyewaanScreenState extends State<RiwayatPenyewaanScreen> {
 
   ValueNotifier<bool> isLoadingValueNotifier = ValueNotifier(true);
+  ValueNotifier<int> riwayatPageValueNotifier = ValueNotifier(1);
+  bool riwayatExtendable = false;
   List<AbstractPenyewaanModel> listPenyewaanModel = [];
 
   @override
@@ -44,85 +45,128 @@ class _RiwayatPenyewaanScreenState extends State<RiwayatPenyewaanScreen> {
           return child!;
         }
       },
-      child: (listPenyewaanModel.isEmpty) ? 
+      child: Builder(
+        builder: (context) {
+          return (listPenyewaanModel.isEmpty) ? 
 
-          ///If list is empty
-          RefreshIndicator(
-            onRefresh: () async{
-              refresh();
-            },
-            child: LayoutBuilder(
-              builder: (p0context, p1constraint) {
-                return SingleChildScrollView(
-                  physics: const AlwaysScrollableScrollPhysics(),
-                  child: Container(
-                    constraints: BoxConstraints(minHeight: p1constraint.maxHeight),
-                    alignment: Alignment.center,
-                    child: const Text(
-                      'Riwayat anda kosong\nSeperti rasanya ditinggal doi',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: regular,
-                        fontSize: 16,
-                        color: primaryLightestColor,
+              ///If list is empty
+              RefreshIndicator(
+                onRefresh: () async{
+                  refresh();
+                },
+                child: LayoutBuilder(
+                  builder: (p0context, p1constraint) {
+                    return SingleChildScrollView(
+                      physics: const AlwaysScrollableScrollPhysics(),
+                      child: Container(
+                        constraints: BoxConstraints(minHeight: p1constraint.maxHeight),
+                        alignment: Alignment.center,
+                        child: const Text(
+                          'Riwayat anda kosong\nSeperti rasanya ditinggal doi',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontWeight: regular,
+                            fontSize: 16,
+                            color: primaryLightestColor,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                );
-              }
-            ),
-          ) : 
+                    );
+                  }
+                ),
+              ) : 
 
-          ///If list is not empty
-          RefreshIndicator(
-            onRefresh: () async{
-              refresh();
-            },
-            child: ListView.builder(
-              itemCount: listPenyewaanModel.length,
-              padding: const EdgeInsets.all(16),
-              itemBuilder: (context, index) =>  PenyewaanContainer(
-                abstractPenyewaanModel: listPenyewaanModel[index],
-                transparentBackground: true,
-              )
-            ),
-          ),
+              ///If list is not empty
+              RefreshIndicator(
+                onRefresh: () async{
+                  refresh();
+                },
+                child: ValueListenableBuilder(
+                  valueListenable: riwayatPageValueNotifier,
+                  builder: (context, value, child) {
+
+                    return ListView.builder(
+                      itemCount: listPenyewaanModel.length + ((riwayatExtendable) ? 1 : 0),
+                      padding: const EdgeInsets.all(16),
+                      itemBuilder: (context, index) {
+                        
+                        if(index < listPenyewaanModel.length){
+                          return  PenyewaanContainer(
+                            abstractPenyewaanModel: listPenyewaanModel[index],
+                            transparentBackground: true,
+                          );
+                        }
+                        else{
+
+                          extendListView();
+
+                          return const Center(
+                            child: CircularProgressIndicator(
+                              backgroundColor: infoColor,
+                              color: Colors.white,
+                            ),
+                          );
+                        }
+                      }
+                    );
+                  }
+                ),
+              );
+        }
+      ),
     );
   }
 
+  ///Refresh screen
   Future<void> refresh() async{
     isLoadingValueNotifier.value = true;
-
-    ///Add list dummy
-    listPenyewaanModel = [
-      SudahDibayarModel(
-        id: 0,
-        fieldName: "Lapangan #1", 
-        rentDateTime: DateTime.now().add(const Duration(hours: 2)), 
-        durationInt: 2, 
-        createdAtDateTime: DateTime(2023, 9, 23, 22, 45),
-      ),
-
-      MenungguPembayaranModel(
-        id: 0,
-        fieldName: "Lapangan #1", 
-        rentDateTime: DateTime.now().add(const Duration(hours: 2)), 
-        durationInt: 2, 
-        createdAtDateTime: DateTime(2023, 9, 23, 22, 45),
-      )
-    ];
     
-    JSONModel json = await GateService.getRiwayatBookingUser();
-    
+    JSONModel json = await GateService.getRiwayatBookingUser(page: 1);
+
+    ///If statusCode = 200, add the data to listPenyewaanModel,
+    ///clear list, reset riwayatExtendable to false, resetPageValueNotifier to 1,
+    ///and findout if the list is extendable
     if(json.statusCode == 200){
-      listPenyewaanModel = (json.data['item'] as List).map((e) {
+
+      riwayatExtendable = false;
+      riwayatPageValueNotifier.value = 1;
+
+      listPenyewaanModel.clear();
+      List<AbstractPenyewaanModel> tempListPenyewaanModel = (json.data['item'] as List).map((e) {
         return AbstractPenyewaanModel.fromJSON(e as Map<String, dynamic>);
       }).toList();
-      print(json.data['item']);
-      print((json.data['item'] as List).length);
+      listPenyewaanModel.addAll(tempListPenyewaanModel);
+
+      riwayatExtendable = (json.data['total_item'] * int.parse(json.data['current_page']) < json.data['total_all_item'])
+          ? true : false;
+    }
+    else if(context.mounted){
+      ScaffoldMessenger.of(context).showSnackBar(
+        CustomSnackbar(title: json.getErrorToString(), color: error2Color,)
+      );
     }
 
- 
     isLoadingValueNotifier.value = false;
+  }
+
+  ///Extend the listView
+  Future<void> extendListView() async{
+    JSONModel json = await GateService.getRiwayatBookingUser(page: riwayatPageValueNotifier.value + 1);
+
+    if(json.statusCode == 200){
+      List<AbstractPenyewaanModel> tempListPenyewaanModel = (json.data['item'] as List).map((e) {
+        return AbstractPenyewaanModel.fromJSON(e as Map<String, dynamic>);
+      }).toList();
+      listPenyewaanModel.addAll(tempListPenyewaanModel);
+
+      riwayatExtendable = (json.data['total_item'] * int.parse(json.data['current_page']) < json.data['total_all_item'])
+          ? true : false;
+      riwayatPageValueNotifier.value += 1;
+    }
+    else if(context.mounted){
+      ScaffoldMessenger.of(context).showSnackBar(
+        CustomSnackbar(title: json.getErrorToString(), color: error2Color,)
+      );
+    }
   }
 }
